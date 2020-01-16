8d313bd8bb7a3917fe99cdaad205a3ec42e2d1cf
AntPzk90 <44503472+AntPzk90@users.noreply.github.com>
2020-01-16 15:45:00 +0200

    add show-more-btn

--- src/main.js
+++ src/main.js
@@ -9,22 +9,43 @@ import {createSortTemplate} from './components/sort';
 import {FilmsCardsMok} from './mock/card';
 import {FilmsFiltersMok} from './mock/main-navigation';
 
+const SHOWING_FILMS_COUNT_ON_START = 5;
+const SHOWING_FILMS_COUNT_BY_BUTTON = 5;
+
 const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
 };
 
 const header = document.querySelector('.header');
 const main = document.querySelector('.main');
+
 render(header, createProfileRatingTemplate());
+
 const filters = FilmsFiltersMok();
-console.log(filters)
+
 render(main, createMainNavigationTemplate(filters));
 render(main, createSortTemplate());
 render(main, createFilmsContainerTemplate());
+
 const filmsContainer = document.querySelector('.films-list__container');
 const films = FilmsCardsMok();
-for (let i = 0; i < films.length; i++){
-  render(filmsContainer, createFilmCardTemplate(films[i]));
-}
+
 render(main, createLoadMoreBtnTemplate());
+
+let showingFilmsCount = SHOWING_FILMS_COUNT_ON_START;
+films.slice(0, showingFilmsCount)
+  .forEach((film) => render(filmsContainer, createFilmCardTemplate(film)));
+
+const showMoreButton = document.querySelector(`.films-list__show-more`);
+
+showMoreButton.addEventListener(`click`, () => {
+  const prevFilmsCount = showingFilmsCount;
+  showingFilmsCount = showingFilmsCount + SHOWING_FILMS_COUNT_BY_BUTTON;
+
+  films.slice(prevFilmsCount, showingFilmsCount)
+    .forEach((film) => render(filmsContainer, createFilmCardTemplate(film)));
+  if (showingFilmsCount >= films.length) {
+    showMoreButton.remove();
+  }
+});
 
